1afeaf22b9421d022256710eb0999e4d082b2b12
Jobin Scaria <jscaria@ctdi.com>
2016-11-22 15:11:34 -0600

    update

---
+++ Vidly/Controllers/CustomerController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Vidly.Controllers
+{
+    public class CustomerController : Controller
+    {
+        // GET: Customer
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [Route("Customer/Details/{id}")]
+        public ActionResult Details(int? id)
+        {
+            if(id == 1)
+            {
+                ViewBag.Name = "John Smith";
+            }
+            else
+            {
+                ViewBag.Name = "Mary Williams";
+            }
+            return View();
+        }
+    }
+}
